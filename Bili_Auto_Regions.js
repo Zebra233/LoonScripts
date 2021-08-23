996495d4ef70b7d1aaf6e5112df8ea5edfaab29c
Zebra233 <kongkongbiubiu@gmail.com>
2021-08-23 13:13:22 +0800

    Update Bili_Auto_Regions.js

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -76,10 +76,10 @@ let $ = nobyda();
 let run = EnvInfo();
 
 async function SwitchRegion(play) {
-    const Group = $.read('BiliArea_Policy') || '📺 哔哩哔哩'; //Your blibli policy group name.
-    const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
-    const TW = $.read('BiliArea_TW') || '🇨🇳 台湾节点'; //Your Taiwan sub-policy name.
-    const HK = $.read('BiliArea_HK') || '🇭🇰 香港节点'; //Your HongKong sub-policy name.
+    const Group = '📺 哔哩哔哩'; //Your blibli policy group name.
+    const CN = 'DIRECT'; //Your China sub-policy name.
+    const TW = '🇨🇳 台湾节点'; //Your Taiwan sub-policy name.
+    const HK = '🇭🇰 香港节点'; //Your HongKong sub-policy name.
     const current = await $.getPolicy(Group);
     const area = (() => {
         if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(play)) {
